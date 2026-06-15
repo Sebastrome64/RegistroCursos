1039eeae223541a0a40ae394266a72c1e41f477a
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-06-14 21:53:33 -0500

    feat: cree la clase Curso y agregue algunos atributos para el registro de horario

---
+++ src/Curso.java
@@ -0,0 +1,10 @@
+public class Curso {
+
+    private String idCurso;
+    private String nombre;
+    private String horario;
+    private String profesor;
+    private String aula;
+    private int cupos;
+    private int tipo;
+}
