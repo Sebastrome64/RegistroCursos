b3dfed39208bc0f4ea1c1e0e22db735085cd8b8e
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-06-14 21:56:43 -0500

    feat: hice el constructor con su getters y setters

--- src/Curso.java
+++ src/Curso.java
@@ -7,4 +7,39 @@ public class Curso {
     private String aula;
     private int cupos;
     private int tipo;
+
+    public Curso(String idCurso, String nombre, String horario, String profesor, String aula, int cupos, int tipo) {
+        this.idCurso = idCurso;
+        this.nombre = nombre;
+        this.horario = horario;
+        this.profesor = profesor;
+        this.aula = aula;
+        this.cupos = cupos;
+        this.tipo = tipo;
+    }
+    public String getIdCurso() {
+        return idCurso;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getHorario() {
+        return horario;
+    }
+    public String getProfesor() {
+        return profesor;
+    }
+    public String getAula() {
+        return aula;
+    }
+    public int getCupos() {
+        return cupos;
+    }
+    public int getTipo() {
+        return tipo;
+    }
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
 }
